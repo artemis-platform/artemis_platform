9d31d48af13ce6367b61f3fb62b518cfd82ea5dd
Chris Laskey <contact@chrislaskey.com>
2020-07-28 17:12:13 -0400

    Update Supervisor Syntax

--- apps/artemis/lib/artemis/workers/interval_supervisor.ex
+++ apps/artemis/lib/artemis/workers/interval_supervisor.ex
@@ -1,23 +1,24 @@
 defmodule Artemis.IntervalSupervisor do
+  use Supervisor
+
   @moduledoc """
   Starts and supervises interval workers.
   """
-
-  use Supervisor
 
   def start_link(options \\ []) do
     Supervisor.start_link(__MODULE__, :ok, name: options[:name] || __MODULE__)
   end
 
+  @impl true
   def init(:ok) do
     children = [
-      worker(Artemis.Worker.RepoResetOnInterval, [])
+      {Artemis.Worker.RepoResetOnInterval, []}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     options = [strategy: :one_for_one]
 
-    supervise(children, options)
+    Supervisor.init(children, options)
   end
 end
--- apps/artemis_log/lib/artemis_log/workers/interval_supervisor.ex
+++ apps/artemis_log/lib/artemis_log/workers/interval_supervisor.ex
@@ -1,24 +1,25 @@
 defmodule ArtemisLog.IntervalSupervisor do
+  use Supervisor
+
   @moduledoc """
   Starts and supervises interval workers.
   """
-
-  use Supervisor
 
   def start_link(options \\ []) do
     Supervisor.start_link(__MODULE__, :ok, name: options[:name] || __MODULE__)
   end
 
+  @impl true
   def init(:ok) do
     children = [
-      worker(ArtemisLog.Worker.DeleteEventLogsOnInterval, []),
-      worker(ArtemisLog.Worker.DeleteHttpRequestLogsOnInterval, [])
+      {ArtemisLog.Worker.DeleteEventLogsOnInterval, []},
+      {ArtemisLog.Worker.DeleteHttpRequestLogsOnInterval, []}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     options = [strategy: :one_for_one]
 
-    supervise(children, options)
+    Supervisor.init(children, options)
   end
 end
